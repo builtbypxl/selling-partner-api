572aee4e6c91e2e46eca0b031b613e2376609b7c
Steve Hitchman <steve@builtbypxl.uk>
2024-04-23 14:43:09 +0100

    correct placement options

--- src/Seller/FBAInboundV20240320/Responses/ListPlacementOptionsResponse.php
+++ src/Seller/FBAInboundV20240320/Responses/ListPlacementOptionsResponse.php
@@ -4,15 +4,16 @@ namespace SellingPartnerApi\Seller\FBAInboundV20240320\Responses;
 
 use Crescat\SaloonSdkGenerator\BaseResponse;
 use SellingPartnerApi\Seller\FBAInboundV20240320\Dto\Pagination;
+use SellingPartnerApi\Seller\FBAInboundV20240320\Dto\PlacementOption;
 use SellingPartnerApi\Seller\FBAInboundV20240320\Dto\PlacementOptionSummary;
 
 final class ListPlacementOptionsResponse extends BaseResponse
 {
-	protected static array $complexArrayTypes = ['placementOptions' => [PlacementOptionSummary::class]];
+	protected static array $complexArrayTypes = ['placementOptions' => [PlacementOption::class]];
 
 
 	/**
-	 * @param PlacementOptionSummary[] $placementOptions Placement options for the inbound plan. This property will be populated when it has been generated via the corresponding endpoint. If there is a chosen placement option, that will be the only returned option. Query the placement option for more details.
+	 * @param PlacementOption[] $placementOptions Placement options for the inbound plan. This property will be populated when it has been generated via the corresponding endpoint. If there is a chosen placement option, that will be the only returned option. Query the placement option for more details.
 	 * @param ?Pagination $pagination Contains tokens to fetch from a certain page.
 	 */
 	public function __construct(
